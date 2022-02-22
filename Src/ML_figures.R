887584b0a16986479697fc68bb468107e7e9c583
Isabelle Moscardini <47124294+IsaMoscardini@users.noreply.github.com>
2022-02-22 10:54:21 +0100

    Figures script added

--- Src/ML_figures.R
+++ Src/ML_figures.R
@@ -1,3 +1,22 @@
+# filtrar tabelas
+genes <- read.csv("Data/Genes_bio (2).csv")
+View(genes)
+
+ff <- list.files(path="Data/RNAseq/", full.names=TRUE)
+tabelas <- lapply(ff, read.delim) %>% setNames(basename(ff))
+View(tabelas[[1]])
+
+for (i in names(tabelas)){
+  tabela = tabelas[[i]]
+  tabela <- tabela[tabela$Probes %in% genes$x,]
+  write.csv2(tabela, paste0('filtered_',i), row.names = TRUE)
+}
+
+
+tab <- read.csv2("filtered_RNAseq_logCPM_D3vD0.txt")
+View(tab)
+
+#####
 rm(list = ls())
 options(stringsAsFactors = F)
 library(mixOmics)
@@ -8,38 +27,42 @@ library(reshape2)
 library(ggpubr)
 
 ## set phenotype
-phenoFile <- "Data/Outcomes.txt"
+phenoFile <- "Intermed/data_BioFeats_USA/final_data/pheno_USA_d1.txt"
 # read phenotype
 phenoData <- read.table(phenoFile, header=T, row.names=1, sep="\t")
-phenoData$AdverseEvent_Class <- NULL
+phenoData$AntibodyResponse_Class <- NULL
 phenoData$name <- rownames(phenoData)
 colnames(phenoData) <- c( "class", "name")
 phenoData <- phenoData[complete.cases(phenoData$class),]
-View(phenoData)
+#View(phenoData)
 
 
 #####
 ## set data file
-dataFile <- "Data/D1/RNAseq_logCPM_D1vD0.txt"
+dataFile <- "Intermed/data_BioFeats_USA/final_data/norm_counts_d1.txt"
 # read data
 df <- as.matrix(read.table(dataFile, header=T, row.names=1, sep="\t"))
 View(df)
-#df <- as.data.frame(df)
-#names <- rownames(df)
-#df <- apply(df, 2, as.numeric)
+df <- as.data.frame(df)
+names <- rownames(df)
+df <- apply(df, 2, as.numeric)
 #names <- make.names(names)
-#rownames(df) <- names
+#names <- gsub("X.Tryptophan", "Tryptophan", names)
+rownames(df) <- names
 
 
 ## selected features
-features <- read.csv("Results/Selected_features_higher_0/Ab/RNAseq_logCPM_D1vD0.csv")
+features <- read.csv("Results/BioFeatS_USA/table_features_metrics.csv")
 View(features)
-probes <- features$Probes
+features <- features[features$Sum > 0,]
+probes <- features$Unnamed..0
 
 #probes <- make.names(features$Probes)
-#View(probes)
+View(probes)
 
 # Filter data
+df <- as.data.frame(df)
+#df$Genes <- rownames(df)
 df <- df[probes, intersect(rownames(phenoData),colnames(df))]
 View(df)
 
@@ -48,24 +71,25 @@ sizeList <- 2:length(features)
 df <- t(df)
 
 pheno <- phenoData[rownames(df),]
-my_colors <- ifelse(pheno$class == "Persistent", "coral1", "lightseagreen")
-AB_response <- pheno$class
-#View(df)
+my_colors <- ifelse(pheno$class == "D1", "coral1", "lightseagreen")
+DAY <- pheno$class
+View(df)
 
-###### FUNCTIONS ###### 
+#
+###### FUNCTION PCA ###### 
 make_PCA <- function(exprs, labels, n=s, outname) {
   exprs <- exprs[,1:n]
   pc <- as.data.frame(pca(exprs)$x)
-  p <- ggplot(pc, aes(x = PC1, y = PC2, col= AB_response)) + geom_point(size=2) + 
+  p <- ggplot(pc, aes(x = PC1, y = PC2, col= AE)) + geom_point(size=2) + 
     theme_minimal() + theme_bw() + labs(title = outname)  + 
     scale_color_manual(values = c("coral1", "lightseagreen")) + theme(legend.position = "top")
   plot(p)
 }
 
 ###### Analysis #####
-pdf("Results/Selected_features_higher_0/AB_miRNA_D7_PCA.pdf", width=6, height=5)
+pdf("Results/Selected_features_higher_0/AE_Olink_D7_PCA.pdf", width=6, height=5)
 for (s in sizeList){
-    outname <- paste("Top", s, "miRNAs")
+    outname <- paste("Top", s, "Proteins")
     make_PCA(exprs = df, labels =  pheno$name, n = s, outname)
   }   
 dev.off()
@@ -73,20 +97,20 @@ dev.off()
 
 #
 
-###########
+###### FUNCTION BOX '####
 
 make_BOX <- function(exprs, labels, n=s, outname) {
   exprs <- as.data.frame(t(exprs))
   exprs$gene <- rownames(exprs)
   mel <- melt(exprs)
   mel$group <- pheno$class[match(mel$variable, pheno$name)]
-  AB_response <- mel$group
-  p <- ggplot(mel, aes(x = reorder(mel$gene, -value), y = mel$value, col = AB_response)) + 
+  AE_response <- mel$group
+  p <- ggplot(mel, aes(x = reorder(mel$gene, -value), y = mel$value, col = AE_response)) + 
     stat_compare_means(method = "wilcox.test", paired = FALSE) + geom_boxplot() + 
-    geom_jitter() + labs(title = "Antibody response RNAseq D1") + xlab("Genes") + ylab("Expression value")
+    geom_jitter() + labs(title = "AE olink D7") + xlab("Proteins") + ylab("Value")
   plot(p)
 }
 
-pdf("Results/Selected_features_higher_0/AB_RNAseq_D1_BOX.pdf", width = 90, height = 30)  
-make_BOX(df, exprs = df, labels =  pheno$name)
-dev.off()
+
+###### Analysis ######
+
